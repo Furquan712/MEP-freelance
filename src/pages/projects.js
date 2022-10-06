f6fd13138a8157090f75e0be90e35ff328985d5a
GUNEET SINGH <guneetsinghtuli@gmail.com>
2022-10-07 00:10:56 +0530

    modal added

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,18 +3,91 @@ import { StickyProvider } from "contexts/app/app.provider";
 import theme from "theme";
 import SEO from "components/seo";
 import Layout from "components/layout";
-import ContactSec from "sections/contact-sec";
+import { FaTimes } from "react-icons/fa";
 import ProjectSec from "sections/project-sec";
+import { Container, Box, Heading, Card, Image, Text } from "theme-ui";
+import { useState } from "react";
+import Img from '../assets/dubai_mall.webp';
 
 export default function Project() {
+  const [popUp, setpopUp] = useState(false);
+  const [popUp2, setpopUp2] = useState(false);
+  const [popUp3, setpopUp3] = useState(false);
+
+  const reset = () => {
+    setpopUp(false);
+    setpopUp2(false);
+    setpopUp3(false);
+  };
   return (
     <ThemeProvider theme={theme}>
-      <StickyProvider>
-        <Layout>
-          <SEO title="Startup Landing 004" />
-          <ProjectSec />
-        </Layout>
-      </StickyProvider>
+      {popUp || popUp2 || popUp3 ? (
+        <Container sx={style.complete}>
+          <Container sx={style.head}>
+            <Box>
+              <Heading as="h1">{popUp && "Mechanical"}{popUp2 && "Electrical"}{popUp3 && "Plumbing"}</Heading>
+            </Box>
+            <Box onClick={reset}>
+              <FaTimes />
+            </Box>
+          </Container>
+          <Container sx={style.body}>
+            <Box sx={style.collectiveCard}>
+              <Card sx={style.card}>
+                <Image src={Img} />
+                <Text>Mall</Text>
+              </Card>
+              <Card sx={style.card}>
+                <Image src={Img} />
+                <Text>Mall</Text>
+              </Card>
+              <Card sx={style.card}>
+                <Image src={Img} />
+                <Text>Mall</Text>
+              </Card>
+              <Card sx={style.card}>
+                <Image src={Img} />
+                <Text>Mall</Text>
+              </Card>
+              <Card sx={style.card}>
+                <Image src={Img} />
+                <Text>Mall</Text>
+              </Card>
+            </Box>
+          </Container>
+          <Container></Container>
+        </Container>
+      ) : (
+        <StickyProvider>
+          <Layout>
+            <SEO title="Startup Landing 004" />
+            <ProjectSec
+              setpopUp={setpopUp}
+              setpopUp2={setpopUp2}
+              setpopUp3={setpopUp3}
+            />
+          </Layout>
+        </StickyProvider>
+      )}
     </ThemeProvider>
   );
 }
+
+const style = {
+  complete:{
+    padding: "40px",
+  },
+  head: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  card:{
+    width:["500px","316px"],
+    margin:"20px",
+  },
+  collectiveCard:{
+    display: "flex",
+    flexWrap: "wrap",
+  }
+};
--- src/sections/project-sec.js
+++ src/sections/project-sec.js
@@ -1,28 +1,41 @@
-/** @jsx jsx */
-import { FaEnvelope, FaPhone } from "react-icons/fa";
-import { jsx } from "theme-ui";
 import { Container, Box, Heading, Text, Row } from "theme-ui";
 import Img from "../assets/elec.jpg";
 import { Image } from "theme-ui";
 import Mall from "../assets/dubai_mall.webp";
-import Metro from '../assets/dubai-metro.jpg'
+import Metro from "../assets/dubai-metro.jpg";
 
-export default function ProjectSec() {
+
+export default function ProjectSec({setpopUp, setpopUp2, setpopUp3}) {
   return (
     <>
       <Image src={Img} sx={styles.Image} />
       <Container sx={styles.contactform}>
-            <div sx={styles.circle}></div>
-            <Heading sx={styles.h1}>USBC MEP</Heading>
+        <div sx={styles.circle}></div>
+        <Heading sx={styles.h1}>USBC MEP</Heading>
       </Container>
       <Container sx={styles.center}>
-        <Heading sx={styles.projectHeading} as="h1" >Our Projects</Heading>
-        <div sx={styles.anotherFlex}>
-          <Heading sx={{...styles.mainText}} as="h2" >M</Heading>
-          <Heading sx={{...styles.mainText,backgroundImage:`url(${Mall})`}} as="h2" >E</Heading>
-          <Heading sx={{...styles.mainText,backgroundImage:`url(${Metro})`}} as="h2" >P</Heading>
+        <Heading sx={styles.projectHeading} as="h1">
+          Our Projects
+        </Heading>
+        <div sx={styles.anotherFlex} style={{ display: "flex" }}>
+          <Heading sx={styles.mainText} as="h2" onClick={() => setpopUp(true)}>
+            M
+          </Heading>
+          <Heading
+            sx={{ ...styles.mainText, backgroundImage: `url(${Mall})` }}
+            as="h2"
+            onClick={() => setpopUp2(true)}
+          >
+            E
+          </Heading>
+          <Heading
+            sx={{ ...styles.mainText, backgroundImage: `url(${Metro})` }}
+            as="h2"
+            onClick={() => setpopUp3(true)}
+          >
+            P
+          </Heading>
         </div>
-        
       </Container>
     </>
   );
@@ -30,47 +43,58 @@ export default function ProjectSec() {
 
 const styles = {
   h1: {
-    fontSize: "90px",
+    fontSize: ["43px", "90px"],
     color: "#F4F8FC",
     letterSpacing: "0.13em",
     fontWeight: "700",
   },
-  center:{
-    display:"flex",
-    justifyContent:"center",
-    alignItems:"center",
-    flexDirection:"column",
+  center: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
   },
-  anotherFlex:{
-    display:"flex",
-    justifyContent:"center",
-    alignItems:"center",
+  blackScreen: {
+    width: "100vw",
+    height: "100vh",
+    backgroundColor: "black",
+    zIndex: "999999",
+    margin: "0",
+    padding: "0",
   },
-  mainText:{
-    fontSize: "433px",
+  anotherFlex: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  mainText: {
+    fontSize: ["50px", "433px"],
     backgroundImage: `url(${Img})`,
     backgroundRepeat: "no-repeat",
     "-webkit-background-clip": "text",
     backgroundClip: "text",
     color: "transparent",
+    cursor: "pointer",
   },
-  circle:{
-    width:'50px',
-    height:'50px',
-    borderRadius:'50%',
-    background: "radial-gradient(70% 70% at 70% 64.8%, rgba(92, 118, 255, 0.6) 0%, rgba(255, 255, 255, 0.5) 100%)",
-    position:"absolute",
-    bottom:"150px",
-    left:"0"
+  circle: {
+    width: "50px",
+    height: "50px",
+    borderRadius: "50%",
+    background:
+      "radial-gradient(70% 70% at 70% 64.8%, rgba(92, 118, 255, 0.6) 0%, rgba(255, 255, 255, 0.5) 100%)",
+    position: "absolute",
+    bottom: "150px",
+    left: "0",
   },
   projectHeading: {
-    fontSize: "133px",
-    color: "#353535"
+    fontSize: ["42px", "133px"],
+    color: "#353535",
   },
   contactform: {
     padding: "0",
-    display: 'flex',
-    justifyContent: 'center',
+    display: "flex",
+    justifyContent: "center",
   },
   salesBox: {
     padding: "10px 20px",
@@ -79,7 +103,7 @@ const styles = {
     width: "100%",
     height: "400px",
     objectFit: "cover",
-    margin:"100px 0 50px 0"
+    margin: "100px 0 50px 0",
   },
   box: {
     padding: "10px 20px",
