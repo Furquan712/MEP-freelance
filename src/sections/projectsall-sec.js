0b3ec62c0db7a3df4b501390db0de866044aff13
Furquan712 <smfurquan7.12@gmail.com>
2022-11-11 01:35:47 +0530

    img updated

--- src/sections/projectsall-sec.js
+++ src/sections/projectsall-sec.js
@@ -2,12 +2,12 @@ import SectionHeader from "components/section-header";
 import { Container, Button, Box, Flex, Card, Image } from "theme-ui";
 import { FaArrowCircleRight } from "react-icons/fa";
 
-import img6 from "assets/img/Atlantis, The Palm.jpg";
+import img6 from "assets/img/AtlantisThePalm.jpg";
 import img7 from "assets/img/Office.jpg";
 import img1 from "assets/img/DWTC.jpg";
-import img2 from "assets/img/Dxb Airport Concorde D.jpg";
-import img5 from "assets/img/Office in Business Bay.jpg";
-import img3 from "assets/img/Maui Restaurant.jpg";
+import img2 from "assets/img/DxbAirportConcordeD.jpg";
+import img5 from "assets/img/OfficeinBusinessBay.jpg";
+import img3 from "assets/img/MauiRestaurant.jpg";
 import img4 from "assets/img/Nail-paint bar.jpg";
 import img8 from "assets/img/residentialTower.jpg";
 import img9 from "assets/img/Sofitel.jpg";
